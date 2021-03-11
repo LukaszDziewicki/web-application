8f72c6955c2e33857915e75791ef8ff4419ca94e
LukaszDziewicki <39883140+LukaszDziewicki@users.noreply.github.com>
2021-03-11 12:31:09 +0100

    Edit:  application.yml  AppUserService  EmailService

--- src/main/java/com/example/storeApplication/email/EmailService.java
+++ src/main/java/com/example/storeApplication/email/EmailService.java
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+@Service
 @AllArgsConstructor
 public class EmailService implements EmailSender {
 
@@ -26,6 +28,7 @@ public class EmailService implements EmailSender {
         helper.setTo(to);
         helper.setSubject("Confirm your email");
         helper.setFrom("shoes_shop@gmail.com");
+        mailSender.send(mimeMessage);
     }catch (MessagingException e){
         LOGGER.error("failed to send email", e);
         throw new IllegalStateException("failed to send email");
